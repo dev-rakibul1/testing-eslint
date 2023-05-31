3b71439ed7b883979f289a9ec25072121a0dc8ed
Your Name <you@example.com>
2023-06-01 00:21:47 +0600

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
-import express, { Application, Request } from 'express'
+import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 dotenv.config()
 
